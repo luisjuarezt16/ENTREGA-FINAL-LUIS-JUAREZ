d5fbf77ac2a124d260aedba1b672d9bcde2b7396
luisjuarezt16 <luisjuarezt16@gmail.com>
2025-11-05 12:37:18 -0300

    try, catch, then

--- js/login.js
+++ js/login.js
@@ -4,10 +4,51 @@ const contraseña = document.getElementById ("contraseña")
 const btnlogin = document.getElementById ("btningresar")
 
 
-btnlogin.addEventListener ("click", () => {
 
-    if (usuario.value === "admin" && contraseña.value ==="12345") {
-        location.href = "../pages/reservas.html"
+const URL = "../db/data.json"
+
+async function obtenerUsuarios() {
+  const response = await fetch(URL)
+  const data = await response.json()
+  return data
+}
+
+
+btnlogin.addEventListener ("click", async (e) => {
+
+
+  try {
+    const usuarios = await obtenerUsuarios()
+
+    const userFound = usuarios.find(
+      (u) =>
+        String(u.usuario) === usuario.value &&
+        String(u.contraseña) === contraseña.value
+    )
+
+    if (userFound) {
+      location.href = "../pages/reservas.html"
+    } else {
+        Swal.fire({
+            title: "Usuario o contraseña incorrectos",
+            icon: "error",
+            showConfirmButton: false,
+            timer: 2000,
+            background: "#330000",
+            color: "#ffffff",
+        })
     }
+
+  } catch (error) {
+    Swal.fire({
+        title: "Error al validar usuario",
+        text: "No se pudo conectar con el servidor, intenta mas tarde.",
+        icon: "error",
+        showConfirmButton: false,
+        timer: 4000,
+        background: "#330000",
+        color: "#ffffff",
+    })
+  } 
 
 })
